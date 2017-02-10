7be0643a29c688f14904a4bfa995ddd7f2be6f93
Kyromyr <Chyromyr@gmail.com>
2017-02-10 17:50:07 +0100

    x64 build no longer works

--- Compile.bat
+++ Compile.bat
@@ -1,8 +1,8 @@
 IF EXIST D2Stats.exe (
 	Del D2Stats.exe /Q
 )
-IF EXIST D2Stats-64.exe (
-	Del D2Stats-64.exe /Q
-)
+REM IF EXIST D2Stats-64.exe (
+REM 	Del D2Stats-64.exe /Q
+REM )
 "Assets/Aut2Exe.exe" /in D2Stats.au3 /out D2Stats.exe /icon "Assets/icon.ico" /x86
-"Assets/Aut2Exe.exe" /in D2Stats.au3 /out D2Stats-64.exe /icon "Assets/icon.ico" /x64
+REM "Assets/Aut2Exe.exe" /in D2Stats.au3 /out D2Stats-64.exe /icon "Assets/icon.ico" /x64
